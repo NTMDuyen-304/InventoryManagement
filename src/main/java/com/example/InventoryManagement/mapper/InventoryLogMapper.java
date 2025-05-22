f9133c8325e12a7986cfcf9e4ceac371f36b11ca
Huyhtq <huyyhuynh2325@gmail.com>
2025-05-22 07:41:01 +0700

    hmmm

--- src/main/java/com/example/InventoryManagement/mapper/InventoryLogMapper.java
+++ src/main/java/com/example/InventoryManagement/mapper/InventoryLogMapper.java
@@ -2,9 +2,11 @@ package com.example.InventoryManagement.mapper;
 
 import com.example.InventoryManagement.dto.InventoryLogDTO;
 import com.example.InventoryManagement.entity.InventoryLog;
+import com.example.InventoryManagement.entity.Product;
 
 public class InventoryLogMapper {
 
+    //entity->dto
     public static InventoryLogDTO toDTO(InventoryLog log) {
         if (log == null) return null;
 
@@ -16,5 +18,24 @@ public class InventoryLogMapper {
                 log.getAmount(),
                 log.getCreatedAt()
         );
+    }
+
+    public static InventoryLog toEntity(InventoryLogDTO dto){
+        if (dto == null) return null;
+
+        InventoryLog log = new InventoryLog();
+        log.setId(dto.getId());
+        log.setAction(dto.getAction());
+        log.setAmount(dto.getAmount());
+        log.setCreatedAt(dto.getCreatedAt());
+
+        if (dto.getProductId() != null) {
+            Product product = new Product();
+            product.setId(dto.getProductId());
+            // Không cần set name vì chỉ cần id để liên kết (nếu cần set name thì dùng khi tạo mới Product)
+            log.setProduct(product);
+        }
+
+        return log;
     }
 }
